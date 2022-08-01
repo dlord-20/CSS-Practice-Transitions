196a0491d210ccbeba9477584906d3f108f8e360
dlord-20 <dthomaslord@gmail.com>
2022-08-01 21:00:59 +0300

    Better looking box-slider with the bottom staying level with the others on hover and the boxes 'growing' higher

--- box-sliders.js
+++ box-sliders.js
@@ -1,6 +1,10 @@
 const boxes = document.querySelectorAll('.box');
+const boxImages = document.querySelectorAll('.box-img');
+const boxTexts = document.querySelectorAll('.box-text');
 
 console.log(boxes);
+console.log(boxImages);
+console.log(boxTexts);
 
 const addTopStyle = (element) => {
     element.style.top = '-12.5px';
@@ -24,14 +28,20 @@ const handleHoverEnter = (e) => {
     const zero = (e === 0);
     const max = (e === boxes.length - 1);
 
-    boxes[e].style.top = '-35px';
+    boxes[e].style.top = '-25px';
+    boxImages[e].style.height = '175px';
+    boxTexts[e].style.height = '200px'
     if(!zero) {
         addTopStyle(boxes[e - 1]);
         addTransparency(boxes[e - 1], adjacentTransparency);
+        boxImages[e - 1].style.height = '165px';
+        boxTexts[e - 1].style.height = '185px';
     }
     if(!max) {
         addTopStyle(boxes[e + 1]);
         addTransparency(boxes[e + 1], adjacentTransparency);
+        boxImages[e + 1].style.height = '165px';
+        boxTexts[e + 1].style.height = '185px';
     }
 
     boxes.forEach((element, index) => {
@@ -45,6 +55,12 @@ const handleHoverExit = (e) => {
     boxes.forEach((element) => {
         removeTopStyle(element);
         removeTransparency(element);
+    })
+    boxImages.forEach((element) => {
+        element.style.height = '150px';
+    })
+    boxTexts.forEach((element) => {
+        element.style.height = '175px';
     })
 }
 
