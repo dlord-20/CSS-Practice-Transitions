0ffc080b2c8f70bf7f834ec26bd4841c30c8b45f
dlord-20 <dthomaslord@gmail.com>
2022-08-02 10:25:27 +0300

    Changed the box-slider so the fonts would adjust accordingly. Looking great honestly ->  now time to clean up the code

--- box-sliders.js
+++ box-sliders.js
@@ -1,10 +1,14 @@
 const boxes = document.querySelectorAll('.box');
 const boxImages = document.querySelectorAll('.box-img');
 const boxTexts = document.querySelectorAll('.box-text');
+const boxHeaders = document.querySelectorAll('.box-head');
+const boxDescriptions = document.querySelectorAll('.box-description');
 
 console.log(boxes);
 console.log(boxImages);
 console.log(boxTexts);
+console.log(boxHeaders);
+console.log(boxDescriptions);
 
 const addTopStyle = (element) => {
     element.style.top = '-12.5px';
@@ -32,12 +36,16 @@ const handleHoverEnter = (e) => {
     boxes[e].style.width = '250px';
     boxImages[e].style.height = '175px';
     boxTexts[e].style.height = '200px'
+    boxHeaders[e].style.fontSize = '1.35rem';
+    boxDescriptions[e].style.fontSize = '1.1rem';
     if(!zero) {
         addTopStyle(boxes[e - 1]);
         addTransparency(boxes[e - 1], adjacentTransparency);
         boxes[e - 1].style.width = '220px';
         boxImages[e - 1].style.height = '165px';
         boxTexts[e - 1].style.height = '185px';
+        boxHeaders[e - 1].style.fontSize = '1.15rem';
+        boxDescriptions[e - 1].style.fontSize = '1.05rem';
     }
     if(!max) {
         addTopStyle(boxes[e + 1]);
@@ -45,6 +53,8 @@ const handleHoverEnter = (e) => {
         boxes[e + 1].style.width = '220px';
         boxImages[e + 1].style.height = '165px';
         boxTexts[e + 1].style.height = '185px';
+        boxHeaders[e + 1].style.fontSize = '1.15rem';
+        boxDescriptions[e + 1].style.fontSize = '1.05rem';
     }
 
     boxes.forEach((element, index) => {
@@ -65,6 +75,12 @@ const handleHoverExit = (e) => {
     })
     boxTexts.forEach((element) => {
         element.style.height = '175px';
+    })
+    boxHeaders.forEach((element) => {
+        element.style.fontSize = '1.1rem';
+    })
+    boxDescriptions.forEach((element) => {
+        element.style.fontSize = '1rem';
     })
 }
 
