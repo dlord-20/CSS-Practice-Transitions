99996341d7abfacd10e930be8c61a16b08d77ba8
dlord-20 <dthomaslord@gmail.com>
2022-08-02 16:12:24 +0300

    Added a new test that doesn't use a container for the effect -> Needs to be tested to see if works if I add it in different sections etc.

--- scroll-reveal.js
+++ scroll-reveal.js
@@ -1,37 +1,28 @@
 const revealItem = (item) => {
-    // var windowHeight = window.innerHeight;
-    // var elementTop = item.getBoundingClientRect().top;
-    // var elementVisible = 200;
-
-    // if (elementTop < windowHeight - elementVisible) {
       item.classList.add("active");
-    // } else {
-    //   item.classList.remove("active");
-    // }
-    
 }
 
 const disappearItem = (item) => {
     item.classList.remove("active");
 }
 
-const reveal = () => {
-    var reveals = document.querySelectorAll(".reveal-item");
+const reveal = (string) => {
+    var reveals = document.querySelectorAll(string);
     let time = 500;
     // setTimeout(() => revealItem(reveals), 2000);
     for (var i = 0; i < reveals.length; i++) {
         const item = reveals[i];
         setTimeout(() => revealItem(item), time);
-        time *= 1.2;
+        time *= 1.15;
     }
 }
 
-const disappear = () => {
-    var reveals = document.querySelectorAll(".reveal-item");
+const disappear = (string) => {
+    var reveals = document.querySelectorAll(string);
     for (var i = 0; i < reveals.length; i++) {
         const item = reveals[i];
         disappearItem(item);
-    }
+    }   
 
 }
 
@@ -45,13 +36,40 @@ const revealContainer = () => {
     
         if (elementTop < windowHeight - elementVisible) {
           item.classList.add("reveal-active");
-          reveal();
+          reveal('.reveal-item');
         } else {
           item.classList.remove("reveal-active");
-          disappear();
+          disappear('.reveal-item');
         }
     })
 
 }
 
 window.addEventListener("scroll", () => revealContainer());
+
+const itemReveal = (item) => {
+    var windowHeight = window.innerHeight;
+    var elementTop = item.getBoundingClientRect().top;
+    var elementVisible = 200;
+
+    if (elementTop < windowHeight - elementVisible) {
+      revealItem(item);
+    } else {
+      disappearItem(item);
+    }
+}
+
+const revealSecondGroup = () => {
+    const containers = document.querySelectorAll(".reveal-test");
+    var time = 500;
+    containers.forEach((container) => {
+        const item = container;
+        setTimeout(() => itemReveal(item), time);
+        time *= 1.15;
+        
+    })
+
+}
+
+
+window.addEventListener("scroll", () => revealSecondGroup());
