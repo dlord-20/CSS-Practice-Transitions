6448f27d7b8e11cb94e8778b334f7783dc6ac0b9
dlord-20 <dthomaslord@gmail.com>
2022-08-02 14:47:20 +0300

    Getting closer to figuring out how to make appearing not buggy but disappear function not working properly

--- scroll-reveal.js
+++ scroll-reveal.js
@@ -1,33 +1,53 @@
 const revealItem = (item) => {
+    // var windowHeight = window.innerHeight;
+    // var elementTop = item.getBoundingClientRect().top;
+    // var elementVisible = 200;
+
+    // if (elementTop < windowHeight - elementVisible) {
+      item.classList.add("active");
+    // } else {
+    //   item.classList.remove("active");
+    // }
+    
+}
+
+const disappearItem = (item) => {
+    item.classList.remove("active");
+}
+
+const reveal = () => {
+    var reveals = document.querySelectorAll(".reveal-item");
+    let time = 500;
+    // setTimeout(() => revealItem(reveals), 2000);
+    for (var i = 0; i < reveals.length; i++) {
+        const item = reveals[i];
+        setTimeout(() => revealItem(item), time);
+        time *= 1.2;
+    }
+}
+
+const disappear = () => {
+    var reveals = document.querySelectorAll(".reveal-item");
+    for (var i = 0; i < reveals.length; i++) {
+        disappearItem(reveals[i]);
+    }
+
+}
+
+const revealContainer = () => {
+    const container = document.querySelectorAll(".reveal-container");
+    const item = container[0];
     var windowHeight = window.innerHeight;
     var elementTop = item.getBoundingClientRect().top;
     var elementVisible = 200;
 
     if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
+      item.classList.add("reveal-active");
+      reveal();
     } else {
-      reveals[i].classList.remove("active");
+      item.classList.remove("reveal-active");
+      disappear();
     }
-    
 }
 
-const reveal = () => {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 200;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-
-
-
-}
-  
-window.addEventListener("scroll", () => reveal());
+window.addEventListener("scroll", () => revealContainer());
